d307e3de00f07ecfed089aff0990a7a53cb8b0ff
SamTheCoder777 <49662629+SamTheCoder777@users.noreply.github.com>
2021-09-11 13:44:28 -0400

    Initial commit

---
+++ src/main/java/com/example/simplenotepad1/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.simplenotepad1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class HelloController {
+    @FXML
+    private Button save;
+
+    @FXML
+    private TextArea textArea;
+
+    public HelloController(){
+
+    }
+
+    @FXML
+    private void initialize(){
+
+    }
+}
