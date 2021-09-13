60023f8f7e4d959b5e332650f8493cd0471401a4
samthecoder <49662629+SamTheCoder777@users.noreply.github.com>
2021-09-13 16:52:54 -0400

    Fixed Scaling and Saving

--- src/main/java/com/example/simplenotepad1/HelloApplication.java
+++ src/main/java/com/example/simplenotepad1/HelloApplication.java
@@ -3,15 +3,16 @@ package com.example.simplenotepad1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
-
+import javafx.geometry.Dimension2D;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Simple Notepad");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/simplenotepad1/HelloController.java
+++ src/main/java/com/example/simplenotepad1/HelloController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class HelloController {
     @FXML
@@ -19,5 +22,25 @@ public class HelloController {
     @FXML
     private void initialize(){
 
+    }
+
+    public void onSave(){
+        String text = textArea.getText().replaceAll("\n", System.getProperty("line.separator"));
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Text");
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showSaveDialog(textArea.getScene().getWindow());
+        if (file != null) {
+            try {
+                PrintWriter writer;
+                writer = new PrintWriter(file);
+                writer.println(text);
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println("");
+            }
+        }
     }
 }
